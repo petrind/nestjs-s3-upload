40bbf21b76728863b94530e607dde1fcc2ac81db
Petrus F. Subekti <petr_ind@yahoo.com>
2021-02-22 04:43:40 +0700

    [feature] AWS, change promise resolve to async/await

--- src/aws/aws.service.ts
+++ src/aws/aws.service.ts
@@ -14,7 +14,7 @@ export class AwsService {
     AWS.config.update({ region: AWS_REGION });
   }
 
-  uploadS3(name, file) {
+  async uploadS3(name, file) {
     const s3: AWS.S3 = new AWS.S3({ apiVersion: AWS_API_VERSION });
     const uploadParams: AwsS3UploadParams = {
       Bucket: S3_BUCKET_NAME,
@@ -24,35 +24,27 @@ export class AwsService {
 
     let status: string;
 
-    return Promise.resolve(
-      s3
-        .upload(uploadParams, (err, data) => {
-          status = FileUploadStatus.WIP;
-          if (err) {
-            // TODO use logger
-            console.log('error', err);
-            status = FileUploadStatus.ERROR_AWS;
-          }
-          if (data) {
-            status = FileUploadStatus.SUCCESS;
-          }
-        })
-        .promise(),
-    )
-      .then(() => {
-        return {
-          filename: name,
-          status,
-        };
-      })
-      .catch((error) => {
-        // TODO use logger
-        console.log('error', error);
-        status = FileUploadStatus.ERROR_IN_PROCESS;
-        return {
-          filename: name,
-          status,
-        };
-      });
+    try {
+      await s3.upload(uploadParams, (err, data) => {
+        status = FileUploadStatus.WIP;
+        if (err) {
+          // TODO use logger
+          console.log('error', err);
+          status = FileUploadStatus.ERROR_AWS;
+        }
+        if (data) {
+          status = FileUploadStatus.SUCCESS;
+        }
+      }).promise()
+    } catch (error) {
+      // TODO use logger
+      console.log('error', error);
+      status = FileUploadStatus.ERROR_IN_PROCESS;
+    }
+
+    return {
+      filename: name,
+      status,
+    };
   }
 }
